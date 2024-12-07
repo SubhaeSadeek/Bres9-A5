bacbfd4a4e5361442beb4e6391b4b48cccd7700b
SubhaeSadeek <nurun.nabi@outlook.com>
2024-12-07 23:09:46 +0600

    blog page design completed

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 const navSection = document.getElementById('nav-section');
 const headerBtnContainer = document.getElementById('header-btn-container')
 
-scrollEffect(navSection, 'bg-transparent');
-scrollEffect(headerBtnContainer, 'bg-[#F9F7F3]/70');
+scrollEffect(navSection, 'bg-[#F9F7F3]/80');
+scrollEffect(headerBtnContainer, 'bg-[#B4F461]/20');
